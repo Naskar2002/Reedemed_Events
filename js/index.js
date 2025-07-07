b4332369df9381972ea53dccae58e3a6c847938c
Naskar2002 <sourovnaskar212gmail.com>
2025-07-07 12:26:24 +0530

    updated

---
+++ js/index.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+  $(".accordion-header").click(() => {
+    $(".accordion-content").slideToggle();
+    $(this).find(".arrow").toggleClass("rotate");
+  });
+});
+
+$(document).ready(function () {
+  $(".accordion-header-2").click(() => {
+    $(".accordion-content-2").slideToggle();
+    $(this).find(".arrow").toggleClass("rotate");
+  });
+});
+
+
+// $(document).ready(function () {
+//   $(".accordion-header-3").click(() => {
+//     $(".accordion-content-3").slideToggle();
+//     $(this).find(".arrow").toggleClass("rotate");
+//   });
+// });
+
+// $(document).ready(function () {
+//   $(".accordion-header-4").click(() => {
+//     $(".accordion-content-4").slideToggle();
+//     $(this).find(".arrow").toggleClass("rotate");
+//   });
+// });
+
+// $(document).ready(function () {
+//   $(".accordion-header-5").click(() => {
+//     $(".accordion-content-5").slideToggle();
+//     $(this).find(".arrow").toggleClass("rotate");
+//   });
+// });
+
+// $(document).ready(function () {
+//   $(".accordion-header-6").click(() => {
+//     $(".accordion-content-6").slideToggle();
+//     $(this).find(".arrow").toggleClass("rotate");
+//   });
+// });
+
+// $(document).ready(function () {
+//   $(".accordion-header-7").click(() => {
+//     $(".accordion-content-7").slideToggle();
+//     $(this).find(".arrow").toggleClass("rotate");
+//   });
+// });
+
+// $(document).ready(function () {
+//   $(".accordion-header-8").click(() => {
+//     $(".accordion-content-8").slideToggle();
+//     $(this).find(".arrow").toggleClass("rotate");
+//   });
+// });
